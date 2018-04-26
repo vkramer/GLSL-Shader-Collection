f638dab878e6320312f6e7772a85b92914af438e
vkramer <vkramer@gmail.com>
2018-04-26 14:13:26 -0400

    Basic Shader

--- Basic/basicFragment.glsl
+++ Basic/basicFragment.glsl
@@ -1,15 +1,8 @@
 #version 330 core
 
-in vec2 texCoordOut;
-
 out vec4 fragColor;
 
-uniform sampler2D sampler;
-
 void main() {
-	vec4 textureColor = texture(sampler, texCoordOut.xy);
-    
-	vec4 color = vec4(1.0, 1.0, 1.0, 1.0);
-
-	fragColor = textureColor * color;
+    // Simple Red Color
+	fragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
